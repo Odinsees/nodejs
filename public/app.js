94bdc0a995460a4d5f50c3d642d064f156777f9d
Lebedev Pavel <pavel.o.lebedev@gmail.com>
2022-05-13 11:02:49 +0300

    add dinamic change basket

--- public/app.js
+++ public/app.js
@@ -1,25 +1,49 @@
-document.querySelectorAll('.price').forEach(node=>{
-  node.textContent = new Intl.NumberFormat('ru-Ru', {
-    currency: 'rub',
-    style: 'currency'
-  }).format(node.textContent)
-})
+const formatCurrency = (price) => {
+  return new Intl.NumberFormat("ru-Ru", {
+    currency: "rub",
+    style: "currency",
+  }).format(price);
+}
 
-const $card = document.querySelector('#card')
 
-if($card){
-  $card.addEventListener('click', event =>{
-    if(event.target.classList.contains('js-remove')){
-      const id = event.target.dataset.id
+document.querySelectorAll(".price").forEach((node) => {
+  node.textContent = formatCurrency(node.textContent)
+});
+
+const $card = document.querySelector("#card");
+
+if ($card) {
+  $card.addEventListener("click", (event) => {
+    if (event.target.classList.contains("js-remove")) {
+      const id = event.target.dataset.id;
       console.log(id);
 
-      fetch('/card/remove/' + id,{
-        method:'delete'
+      fetch("/card/remove/" + id, {
+        method: "delete",
       })
-      .then(res=>res.json())
-      .then(device=>{
-        console.log(device)
-      })
+        .then((res) => res.json())
+        .then((card) => {
+          if (card.devices.length) {
+            const html = card.devices.map(device=> {
+              return `
+              <tr>
+                <td>${device.type}</td>
+                <td>${device.count}</td>
+                <td>
+                  <button
+                  class="btn btn-small js-remove"
+                  data-id="${device.id}"
+                  >Remove</button>
+                </td>
+              </tr>
+              `
+            }).join('')
+            $card.querySelector('tbody').innerHTML = html
+            $card.querySelector('.price').textContent = formatCurrency(card.sumPrice)
+          } else {
+            $card.innerHTML = "<p>Basket empty</p>";
+          }
+        });
     }
-  })
+  });
 }
